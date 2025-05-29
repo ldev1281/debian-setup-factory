7d59648294e53f0e55991e1ff667c190952e2d5d
Andrew Kornilov <akornilov@gmail.com>
2025-05-30 02:11:42 +0300

    Shorten variables

--- setup-modules/tor-transparent-conf.bash
+++ setup-modules/tor-transparent-conf.bash
@@ -14,11 +14,11 @@ TOR_TRANSPARENT_CONF_TRANS_HOST="${TOR_TRANSPARENT_CONF_TRANS_HOST:-127.0.0.1}"
 TOR_TRANSPARENT_CONF_TRANS_PORT="${TOR_TRANSPARENT_CONF_TRANS_PORT:-9040}"
 TOR_TRANSPARENT_CONF_TRANS_OPTS="${TOR_TRANSPARENT_CONF_TRANS_OPTS:-IsolateClientAddr IsolateClientProtocol IsolateDestAddr IsolateDestPort}"
 TOR_TRANSPARENT_CONF_VIRTUAL_NET="${TOR_TRANSPARENT_CONF_VIRTUAL_NET:-10.192.0.0/10}"
-TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME="${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME:=transparent}"
-_TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_DIR="/var/lib/tor-instances/${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}"
-TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_HOST="${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_HOST:=127.0.0.1}"
-TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_PORT="${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_PORT:=80}"
-TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_LISTEN="${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_LISTEN:=80}"
+TOR_TRANSPARENT_CONF_HS_NAME="${TOR_TRANSPARENT_CONF_HS_NAME:=transparent}"
+_TOR_TRANSPARENT_CONF_HS_DIR="/var/lib/tor-instances/${TOR_TRANSPARENT_CONF_HS_NAME}"
+TOR_TRANSPARENT_CONF_HS_HOST="${TOR_TRANSPARENT_CONF_HS_HOST:=127.0.0.1}"
+TOR_TRANSPARENT_CONF_HS_PORT="${TOR_TRANSPARENT_CONF_HS_PORT:=80}"
+TOR_TRANSPARENT_CONF_HS_LISTEN="${TOR_TRANSPARENT_CONF_HS_LISTEN:=80}"
 
 # Check for root
 [ "${EUID:-$(id -u)}" -eq 0 ] || logger::err "Script must be run with root privileges"
@@ -31,7 +31,7 @@ logger::log "Transparent transport tor configuration"
 #
 # Creating new tor instance with a separate config, working directory and user
 #
-tor-instance-create ${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} || "failed to setup tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}"
+tor-instance-create ${TOR_TRANSPARENT_CONF_HS_NAME} || "failed to setup tor@${TOR_TRANSPARENT_CONF_HS_NAME}"
 
 #
 # DNS over tor
@@ -42,16 +42,16 @@ tor-instance-create ${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} || "failed to se
     echo "VirtualAddrNetworkIPv4 ${TOR_TRANSPARENT_CONF_VIRTUAL_NET}"
     echo "AutomapHostsOnResolve 1"
     echo ""
-} >/etc/tor/instances/${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}/torrc
+} >/etc/tor/instances/${TOR_TRANSPARENT_CONF_HS_NAME}/torrc
 
 #
 # Hidden Service
 #
 {
-    echo "HiddenServiceDir ${_TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_DIR}"
-    echo "HiddenServicePort ${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_LISTEN} ${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_HOST}:${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_PORT}"
+    echo "HiddenServiceDir ${_TOR_TRANSPARENT_CONF_HS_DIR}"
+    echo "HiddenServicePort ${TOR_TRANSPARENT_CONF_HS_LISTEN} ${TOR_TRANSPARENT_CONF_HS_HOST}:${TOR_TRANSPARENT_CONF_HS_PORT}"
     echo ""
-} >>/etc/tor/instances/${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}/torrc
+} >>/etc/tor/instances/${TOR_TRANSPARENT_CONF_HS_NAME}/torrc
 
 # Disable systemd-resolved if enabled to prevent overwriting /etc/resolv.conf
 if systemctl is-active systemd-resolved >/dev/null 2>&1; then
@@ -85,8 +85,8 @@ systemctl daemon-reexec
 systemctl daemon-reload
 systemctl enable nftables || logger::err "Failed to enable nftables service"
 systemctl restart nftables || logger::err "Failed to start nftables service"
-systemctl enable tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} || logger::err "Failed to enable tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} service"
-systemctl restart tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} || logger::err "Failed to start tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} service"
+systemctl enable tor@${TOR_TRANSPARENT_CONF_HS_NAME} || logger::err "Failed to enable tor@${TOR_TRANSPARENT_CONF_HS_NAME} service"
+systemctl restart tor@${TOR_TRANSPARENT_CONF_HS_NAME} || logger::err "Failed to start tor@${TOR_TRANSPARENT_CONF_HS_NAME} service"
 
 #
 # testing tor setup
@@ -101,10 +101,10 @@ while ! curl --silent --fail http://2gzyxa5ihm7nsggfxnu52rck2vv4rvmdlkiu3zzui5du
     if ((_TOR_TRANSPARENT_CONF_DNS_TEST_ATTEMPTS % 6 == 0)); then
         ((_TOR_TRANSPARENT_CONF_DNS_TEST_RESTARTS++))
         if ((_TOR_TRANSPARENT_CONF_DNS_TEST_RESTARTS <= 1)); then
-            logger::log "restarting tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}..."
-            systemctl restart tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} || logger::err "Failed to start tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} service"
+            logger::log "restarting tor@${TOR_TRANSPARENT_CONF_HS_NAME}..."
+            systemctl restart tor@${TOR_TRANSPARENT_CONF_HS_NAME} || logger::err "Failed to start tor@${TOR_TRANSPARENT_CONF_HS_NAME} service"
         else
-            logger::log "failed to setup tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}"
+            logger::log "failed to setup tor@${TOR_TRANSPARENT_CONF_HS_NAME}"
         fi
     fi
 
@@ -115,20 +115,20 @@ done
 # testing hidden service setup
 #
 logger::log "Testing hidden service setup"
-_TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_URL="http://$(cat ${_TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_DIR}/hostname)"
+_TOR_TRANSPARENT_CONF_HS_URL="http://$(cat ${_TOR_TRANSPARENT_CONF_HS_DIR}/hostname)"
 _TOR_TRANSPARENT_CONF_HS_TEST_ATTEMPTS=0
 _TOR_TRANSPARENT_CONF_HS_TEST_RESTARTS=0
-while ! curl --silent --fail ${_TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_URL} >/dev/null; do
+while ! curl --silent --fail ${_TOR_TRANSPARENT_CONF_HS_URL} >/dev/null; do
     ((_TOR_TRANSPARENT_CONF_HS_TEST_ATTEMPTS++))
     logger::log "still waiting hidden service up..."
 
     if ((_TOR_TRANSPARENT_CONF_HS_TEST_ATTEMPTS % 6 == 0)); then
         ((_TOR_TRANSPARENT_CONF_HS_TEST_RESTARTS++))
         if ((_TOR_TRANSPARENT_CONF_HS_TEST_RESTARTS <= 1)); then
-            logger::log "restarting tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}..."
-            systemctl restart tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} || logger::err "Failed to start tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME} service"
+            logger::log "restarting tor@${TOR_TRANSPARENT_CONF_HS_NAME}..."
+            systemctl restart tor@${TOR_TRANSPARENT_CONF_HS_NAME} || logger::err "Failed to start tor@${TOR_TRANSPARENT_CONF_HS_NAME} service"
         else
-            logger::err "failed to setup tor@${TOR_TRANSPARENT_CONF_HIDDEN_SERVICE_NAME}"
+            logger::err "failed to setup tor@${TOR_TRANSPARENT_CONF_HS_NAME}"
         fi
     fi
 
